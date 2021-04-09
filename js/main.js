bda73f97e9aec57a7ff83244c19a0a79237e21d2
Valera Shabalin <shabalinvalera.ts@gmail.com>
2021-04-09 11:26:51 +0700

    Update

--- js/main.js
+++ js/main.js
@@ -1,30 +1,33 @@
-window.addEventListener('load', () => {
-	buttonArrows()
-	openMenu('.header__humb', '.mobile-menu')
-	scrollHeader('.header')
-	checkButton('.btn_up')
+window.addEventListener('DOMContentLoaded', () => {
+	buttonArrowsAdd()
+	initOpenMenu()
+	initScrollHeader()
+	initBtnUp()
 })
 
-function buttonArrows() {
+function buttonArrowsAdd() {
 	let buttonsLite = document.querySelectorAll('.btn_lite'),
 		buttonsBig = document.querySelectorAll('.btn_big')
 
 	buttonsLite.forEach((element) => {
-		element.innerHTML += "<svg xmlns='http://www.w3.org/2000/svg' width='26' height='26' viewBox='0 0 512 512'><title>ionicons-v5-a</title><polyline points='268 112 412 256 268 400' style='fill:none;stroke:#0f7bff;stroke-linecap:round;stroke-linejoin:round;stroke-width:20px'/><line x1='392' y1='256' x2='100' y2='256' style='fill:none;stroke:#0f7bff;stroke-linecap:round;stroke-linejoin:round;stroke-width:20px'/></svg>"
+		element.innerHTML += `<svg width=\"68\" height=\"16\" viewBox=\"0 0 68 16\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">
+									<path d=\"M67.7071 8.70711C68.0976 8.31658 68.0976 7.68342 67.7071 7.29289L61.3431 0.928932C60.9526 0.538408 60.3195 0.538408 59.9289 0.928932C59.5384 1.31946 59.5384 1.95262 59.9289 2.34315L65.5858 8L59.9289 13.6569C59.5384 14.0474 59.5384 14.6805 59.9289 15.0711C60.3195 15.4616 60.9526 15.4616 61.3431 15.0711L67.7071 8.70711ZM0 9H67V7H0V9Z\" fill=\"#0063DD\"/>
+							  </svg>`
 	})
 	buttonsBig.forEach((element) => {
-		element.innerHTML += "<svg xmlns='http://www.w3.org/2000/svg' width='26' height='26' viewBox='0 0 512 512'><title>ionicons-v5-a</title><polyline points='268 112 412 256 268 400' style='fill:none;stroke:#fff;stroke-linecap:round;stroke-linejoin:round;stroke-width:40px'/><line x1='392' y1='256' x2='100' y2='256' style='fill:none;stroke:#fff;stroke-linecap:round;stroke-linejoin:round;stroke-width:40px'/></svg>"
+		element.innerHTML += `<svg width="31" height="16" viewBox="0 0 31 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+									<path d="M30.7071 8.70711C31.0976 8.31659 31.0976 7.68342 30.7071 7.2929L24.3431 0.928934C23.9526 0.53841 23.3195 0.53841 22.9289 0.928934C22.5384 1.31946 22.5384 1.95262 22.9289 2.34315L28.5858 8L22.9289 13.6569C22.5384 14.0474 22.5384 14.6805 22.9289 15.0711C23.3195 15.4616 23.9526 15.4616 24.3431 15.0711L30.7071 8.70711ZM-8.74228e-08 9L30 9L30 7L8.74228e-08 7L-8.74228e-08 9Z" fill="white"/>
+							  </svg>`
 	})
 }
 
-function openMenu(btn, menuBody) {
-	let button = document.querySelector(btn),
-		menu = document.querySelector(menuBody)
+function initOpenMenu() {
+	let button = document.querySelector('.header__humb'),
+		menu = document.querySelector('.mobile-menu')
 
-	button.addEventListener('click', (e) => {
-		if ( e ) {
-			e.preventDefault()
-		}
+	button.addEventListener('click', event => {
+		event.preventDefault()
+
 		if ( !menu.classList.contains('js_active') ) {
 			menu.classList.add('js_active')
 			button.classList.add('js_active')
@@ -37,8 +40,8 @@ function openMenu(btn, menuBody) {
 	})
 }
 
-function scrollHeader(headerItem) {
-	let header = document.querySelector(headerItem)
+function initScrollHeader() {
+	let header = document.querySelector('.header')
 
 	window.addEventListener('scroll', () => {
 		if ( window.pageYOffset > 150 ) {
@@ -49,8 +52,8 @@ function scrollHeader(headerItem) {
 	})
 }
 
-function checkButton(btn) {
-	let button = document.querySelector(btn)
+function initBtnUp(btn) {
+	let button = document.querySelector('.btn_up')
 
 	window.addEventListener('scroll', () => {
 		if ( window.pageYOffset > 300 ) {
